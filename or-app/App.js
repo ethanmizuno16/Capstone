43dbf9323e345b182cbe1b9bf4f4a7f8fc5b6125
Shivesh <shiveshummat@gmail.com>
2024-02-20 10:03:40 -0800

    create app

---
+++ or-app/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to the Remote Monitoring Anesthesia App.</Text>
+      <Text style={styles.description}>
+        Testing text to go here.
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    margin: 20,
+  },
+});
